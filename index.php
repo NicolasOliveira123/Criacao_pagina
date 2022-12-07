a01212ab9a0a8b31a265c9480b8bf70b4d9b8e17
nicolasoliveirareis777@gmail.com <nicolasoliveirareis777@gmail.com>
2022-12-07 17:23:04 -0300

    Criação de pagina inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pagina</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div>
+		<img class="pagina" src="img/132846809SZ.webp" alt="">
+	</div>
+	<br>
+	<tr>Smart Tv Led Android 40 Full HD com Conversor Digital 2 HDMI 1 USB</tr>
+	<br>
+	<tr><h5>vendido e entregue pela Americanas</h5></tr>
+	<tr><h1>R$ 1.299,00</h1></tr>
+	<tr><h6>8x de R$ 162,49 s/ juros  ver parcelas</h6></tr>
+</body>
+</html>
